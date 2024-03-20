ee16c01db9718dcd200e9d1ccec00f39c6945a50
devitoangelo <angelodevito4@gmail.com>
2024-03-20 16:07:06 +0100

    my project Vite Hello World

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+
+// CreateAPP
+export default {
+
+  data() {
+
+    return {
+
+      text: "Hello world"
+
+
+
+    }
+
+
+  }
+
+}
+
+</script>
+
+
+<template>
+<header>
+  <h1>{{ text }}</h1>
+</header>
+
+
+</template>
+
+
+<style>
+h1 {
+  color: white;
+  background-color: black;
+  width: 150px;
+  border-radius: 2rem;
+  text-align: center;
+}
+</style>
